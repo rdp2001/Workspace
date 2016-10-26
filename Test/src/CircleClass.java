10805036180f6b673b6e5eb353863342b881afc6
Paul <richard_paul@hwschools.lan>
2016-10-26 13:23:07 -0400

    Initial commit

---
+++ Test/src/CircleClass.java
@@ -0,0 +1,12 @@
+
+
+public class CircleClass {
+
+	public static void main(String[] args) {
+		Circle Circle = new Circle(5.1);
+		Circle cir2 = new Circle(5.1);
+		System.out.println(Circle.diameter());
+		System.out.println(cir2.circumference());
+	}//end main
+
+}//end of CircleClass
